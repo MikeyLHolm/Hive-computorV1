c952b4175a1f30af1b9766c9aecc8fae744c420f
Mikko Lindholm <mikko.lindholm3@gmail.com>
2020-10-28 15:31:04 +0200

    reduced form ok, just need to sort int/float thing

--- reduced_form.py
+++ reduced_form.py
@@ -1,12 +1,17 @@
+import sys
 
 
 def reduced_form(equation_list):
-    reduced_form = 'Reduced form: '
+    reduced_form = 'Reduced form: ' + str(equation_list[0].coeff) + ' * X^' + str(equation_list[0].degree)
+
+    for term in equation_list[1:]:
+        sign = ' + '
+        if float(term.coeff) < 0:
+            sign = ' - '
+        reduced_form = reduced_form + sign + str(term.coeff) + ' * X^' + str(term.degree)
+
+    reduced_form = reduced_form + ' = 0'
     print(reduced_form)
-    for term in equation_list:
-        
-
-
 
 
 
